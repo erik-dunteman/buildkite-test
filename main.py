eac621c6f85096b1d92bb42545d0d83d3c091ab2
erik-dunteman <erikdunteman@gmail.com>
2024-12-11 13:32:55 -0800

    test

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import os
 
 BUILDKITE_COMMIT = os.getenv("BUILDKITE_COMMIT")
 BUILDKITE_COMMIT = "bfd610430c04d2962a03a2db304fb13b09b4f1b3" # todo: remove this override
-GPU = os.environ["GPU"]
-CMD = os.environ["CMD"]
+GPU = os.getenv("GPU")
+CMD = os.getenv("CMD")
 
 BASE_IMG = f"public.ecr.aws/q9t5s3a7/vllm-ci-postmerge-repo:{BUILDKITE_COMMIT}"
 
