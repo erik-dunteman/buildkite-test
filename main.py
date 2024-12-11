7625edb8b7f8373ab12c6cdf110c6e4e0c39d884
erik-dunteman <erikdunteman@gmail.com>
2024-12-11 13:03:24 -0800

    test

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import subprocess
 import time
 import os
 
+BUILDKITE_COMMIT = os.getenv("BUILDKITE_COMMIT", "bfd610430c04d2962a03a2db304fb13b09b4f1b3") # todo: remove default BUILDKITE_COMMIT
+BASE_IMG = f"public.ecr.aws/q9t5s3a7/vllm-ci-postmerge-repo:{BUILDKITE_COMMIT}"
+
 app = modal.App("buildkite-agent")
 
 image = (
-    modal.Image.debian_slim()
+    modal.Image.from_registry(BASE_IMG, add_python="3.12")
+    .workdir("/vllm-workspace")
     .apt_install(["apt-transport-https", "dirmngr", "curl", "gpg"])
     .run_commands([
         "curl -fsSL https://keys.openpgp.org/vks/v1/by-fingerprint/32A37959C2FA5C3C99EFBC32A79206696452D198 | gpg --dearmor -o /usr/share/keyrings/buildkite-agent-archive-keyring.gpg",
